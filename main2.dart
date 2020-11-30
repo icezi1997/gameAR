1b8442f68738974184fb47661517dbdbc40a50fd
icezi1997 <72901573+icezi1997@users.noreply.github.com>
2020-11-30 10:31:01 +0700

    Add files via upload

---
+++ main2.dart
@@ -0,0 +1,18 @@
+   public void 1Scene(){
+        SceneManager.LoadScene("1-hub-cable");
+    }
+    public void 2Scene(){
+        SceneManager.LoadScene("2-com-cable");
+    }
+    public void 3Scene(){
+        SceneManager.LoadScene("3-hub-com");
+    }
+    public void 4Scene(){
+        SceneManager.LoadScene("4-cable");
+    }
+    public void 5Scene(){
+        SceneManager.LoadScene("5-hub");
+    }
+    public void 6Scene(){
+        SceneManager.LoadScene("6-com");
+    }
